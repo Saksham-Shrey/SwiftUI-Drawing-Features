9504a2eacf83e5a559f9b9ba5adb654470351bb9
Saksham Shrey <110986726+Saksham-Shrey@users.noreply.github.com>
2025-05-21 20:35:49 +0530

    Initial Commit

---
+++ SwiftUI-Drawing-Features/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Drawing-Features
+//
+//  Created by Saksham Shrey on 21/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Drawing-Features/SwiftUI_Drawing_FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Drawing_FeaturesApp.swift
+//  SwiftUI-Drawing-Features
+//
+//  Created by Saksham Shrey on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Drawing_FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
